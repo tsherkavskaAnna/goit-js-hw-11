40ba52f04ab19b03b20646c76f352778def21e80
Anna Tsherkavska <katotoka1985@gmail.com>
2022-04-25 15:04:55 +0200

    update task

--- src/index.js
+++ src/index.js
@@ -2,3 +2,65 @@
 import "./css/style.css"
 import { Notify } from "notiflix"
 import axios from "axios"
+import { getPhotos } from "./js/api";
+
+const inputSearch = document.querySelector(`input`);
+console.log(inputSearch)
+const btnSearch = document.querySelector(`.btn`);
+console.log(btnSearch)
+const gallery = document.querySelector(`.gallery`)
+console.log(gallery)
+
+
+
+let data = ``;
+btnSearch.addEventListener(`submit`, onInputSearch);
+
+function onInputSearch(event) {
+    event.ppreventDefault()
+    const hits = inputSearch.searchQuery.value.trim();
+    console.log(hits)
+
+}
+
+getPhotos(hits).then(renderPhotoCard).catch();
+
+function renderPhotoCard() {
+    gallery.innerHTML = ``;
+    if(name.length === 0) {
+        Notify.failure(`Sorry, there are no images matching your search query. Please try again.`)
+    }
+    return makePhotoCard()
+}
+
+function makePhotoCard({data}) {
+    const markup = data.map(({webformatURL,largeImageURL,tags,likes,views,comments,downloads}) => {
+        return `<div class="photo-card">
+        <a class="gallery-item" href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes: </b></br>${likes}
+          </p>
+          <p class="info-item">
+            <b>Views:</b></br>${views}
+          </p>
+          <p class="info-item">
+            <b>Comments:</b></br>${comments}
+          </p>
+          <p class="info-item">
+            <b>Dowloads:</b></br>${downloads}
+          </p>
+        </div>
+      </div>`
+    }).join(``);
+gallery.innerHTML = markup
+}
+// webformatURL - посилання на маленьке зображення для списку карток.
+// largeImageURL - посилання на велике зображення.
+// tags - рядок з описом зображення. Підійде для атрибуту alt.
+// likes - кількість лайків.
+// views - кількість переглядів.
+// comments - кількість коментарів.
+// downloads - кількість завантажень.
+
+
--- src/js/api.js
+++ src/js/api.js
@@ -1,12 +1,16 @@
 const API_KAY = `26979383-64bf469f69381b7a5cedcba95`;
-const URL_BASE = `https://pixabay.com/api/`;
+const BASE_URL = `https://pixabay.com/api/`;
 const OPTIONS = `q=image_type=photo&orientation=horizontal&safesearch=true`
 
 
-//Example url: ${URL_BASE}?key=${API_KAY}&${OPTIONS}//
+//Example url: ${BASE_URL}?key=${API_KAY}&${OPTIONS}//
 
-// key - твій унікальний ключ доступу до API.
-// q - термін для пошуку. Те, що буде вводити користувач.
-// image_type - тип зображення. На потрібні тільки фотографії, тому постав значення photo.
-// orientation - орієнтація фотографії. Постав значення horizontal.
-// safesearch - фільтр за віком. Постав значення true.
+export const getPhotos = () => {
+    return fetch(`${BASE_URL}?key=${API_KAY}&${OPTIONS}`)
+    .then(response => {
+        if(!response.ok) {
+            throw new Error(response.statusText)
+        }
+        return response.json()
+    })
+}
